8c38f2cbcd070a5f6e1e5ca12cb7deea3bd62f02
Matej Kardy Kardoš <matej.kardos@gmail.com>
2019-03-26 08:09:40 +0100

    First look at the architecture. Keep on working you will make it.

---
+++ EnergyLoss/EnergyLoss/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyLoss
+{
+    public class Author
+    {
+        private string _name;
+        private string _lastName;
+
+        public Author()
+        {
+            _name = "Matej";
+            _lastName = "Kardos";
+        }
+        public Author(string name, string lastName)
+        {
+            _name = name;
+            _lastName = lastName;
+        }
+    }
+}
---
+++ EnergyLoss/EnergyLoss/Building.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyLoss
+{
+    public class Building
+    {
+        private string _name;
+        private Author _author;
+        private Country _country;
+        private BuildingType _buildingType;
+        private double _outsideTemp;
+        private 
+    }
+}
